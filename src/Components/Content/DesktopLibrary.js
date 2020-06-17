f7ed723f6574dd2cbe22e2386ffa57a50e1427ca
tiffany t <iluvpups20@gmail.com>
2020-06-17 16:18:38 -0500

    Card avatars

--- src/Components/Content/DesktopLibrary.js
+++ src/Components/Content/DesktopLibrary.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from "styled-components";
 
-// Image files
+// Card Image files
 import Desktop1 from '../../Images/Desktop1.jpg';
 import Desktop2 from '../../Images/Desktop2.jpg';
 import Desktop3 from '../../Images/Desktop3.jpg';
@@ -10,6 +10,15 @@ import Desktop5 from '../../Images/Desktop5.jpg';
 import Desktop6 from '../../Images/Desktop6.jpg';
 import Desktop7 from '../../Images/Desktop7.jpg';
 import Desktop8 from '../../Images/Desktop8.jpg';
+// Avatar Image files
+import Avatar1 from '../../Images/Desktop1Avatar.jpg';
+import Avatar2 from '../../Images/Desktop2Avatar.jpg';
+import Avatar3 from '../../Images/Desktop3Avatar.jpg';
+import Avatar4 from '../../Images/Desktop4Avatar.jpg';
+import Avatar5 from '../../Images/Desktop5Avatar.jpg';
+import Avatar6 from '../../Images/Desktop6Avatar.jpg';
+import Avatar7 from '../../Images/Desktop7Avatar.jpg';
+import Avatar8 from '../../Images/Desktop8Avatar.jpg';
 
 const Wrapper = styled.section`
     width: 100%;
@@ -43,6 +52,24 @@ const CardImgWrapper = styled.div`
 const CardImg = styled.img`
     min-width: 100%;
 `;
+const CardInfoWrapper = styled.div`
+    padding: 16px;
+    width: 100%;
+    height: 116px;
+    font: 700 16px/18px "Proxima Nova Rg";
+`;
+const Details = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+const Title = styled.p`
+    color: #2e3134;
+    letter-spacing: 0.3px;
+`;
+const Avatar = styled.img`
+    width: 28px;
+    height: 28px;
+`;
 
 function DesktopLibrary() {
     return (
@@ -52,41 +79,89 @@ function DesktopLibrary() {
                 <CardImgWrapper>
                     <CardImg src={Desktop1} alt="Card 1" />
                 </CardImgWrapper>
+                <CardInfoWrapper>
+                    <Details>
+                        <Title>Lake Inniscarra, Ireland—Pyramid</Title>
+                        <Avatar src={Avatar1} alt="Avatar 1" />
+                    </Details>
+                </CardInfoWrapper>
             </Card>
             <Card>
                 <CardImgWrapper>
                     <CardImg src={Desktop2} alt="Card 2" />
                 </CardImgWrapper>
+                <CardInfoWrapper>
+                    <Details>
+                        <Title>Performance Series</Title>
+                        <Avatar src={Avatar2} alt="Avatar 2" />
+                    </Details>
+                </CardInfoWrapper>
             </Card>
             <Card>
                 <CardImgWrapper>
                     <CardImg src={Desktop3} alt="Card 3" />
                 </CardImgWrapper>
+                <CardInfoWrapper>
+                    <Details>
+                        <Title>Slow Pulls and Fast Intervals</Title>
+                        <Avatar src={Avatar3} alt="Avatar 3" />
+                    </Details>
+                </CardInfoWrapper>
             </Card>
             <Card>
                 <CardImgWrapper>
                     <CardImg src={Desktop4} alt="Card 4" />
                 </CardImgWrapper>
+                <CardInfoWrapper>
+                    <Details>
+                        <Title>20 Minutes to Toned</Title>
+                        <Avatar src={Avatar4} alt="Avatar 4" />
+                    </Details>
+                </CardInfoWrapper>
             </Card>
             <Card>
                 <CardImgWrapper>
                     <CardImg src={Desktop5} alt="Card 5" />
                 </CardImgWrapper>
+                <CardInfoWrapper>
+                    <Details>
+                        <Title>Charles Race, Boston, Massachusetts</Title>
+                        <Avatar src={Avatar5} alt="Avatar 5" />
+                    </Details>
+                </CardInfoWrapper>
             </Card>
             <Card>
                 <CardImgWrapper>
                     <CardImg src={Desktop6} alt="Card 6" />
                 </CardImgWrapper>
+                <CardInfoWrapper>
+                    <Details>
+                        <Title>Full-Body HIIT Series</Title>
+                        <Avatar src={Avatar6} alt="Avatar 6" />
+                    </Details>
+                </CardInfoWrapper>
             </Card>
             <Card>
                 <CardImgWrapper>
                     <CardImg src={Desktop7} alt="Card 7" />
                 </CardImgWrapper>
+                <CardInfoWrapper>
+                    <Details>
+                        <Title>Kafue River, Zambia—Power Stroke Pyramid</Title>
+                        <Avatar src={Avatar7} alt="Avatar 7" />
+                    </Details>
+                </CardInfoWrapper>
             </Card>
             <Card>
                 <CardImgWrapper>
                     <CardImg src={Desktop8} alt="Card 8" />
                 </CardImgWrapper>
+                <CardInfoWrapper>
+                    <Details>
+                        <Title>Shred & Burn Series</Title>
+                        <Avatar src={Avatar8} alt="Avatar 8" />
+                    </Details>
+                </CardInfoWrapper>
             </Card>
         </Container>
     </Wrapper>
--- src/Components/Content/Equipment.js
+++ src/Components/Content/Equipment.js
@@ -16,7 +16,7 @@ const Wrapper = styled.section`
     align-items: center;
     justify-content: center;
     font: 600 24px/28px "Proxima Nova Lt";
-    margin: 100px 0 50px 0;
+    margin: 75px 0 50px 0;
 `;
 const Text = styled.p`
     opacity: 0.8;
